f7566c03e65550a76afd9709b974619e2abe3204
Noreen Wu <noreenwu@gmail.com>
2021-09-26 15:35:39 -0400

    fix: delete todos now works from the ui

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -6,26 +6,43 @@ import * as AWS from 'aws-sdk'
 const docClient = new AWS.DynamoDB.DocumentClient();
 const todosTable = process.env.TODOS_TABLE
 
+const deleteTodo = async(todoId: string, userId: string) => {
+  console.log("deleteTodo todostable is ", todosTable)
+  console.log("user id is ", userId)
+  console.log("todo Id is ", todoId)
+  const deleteTodo = await docClient.delete({
+      TableName: todosTable,
+      Key: { userId, todoId }
+  })
+  .promise();
+  return { Deleted: deleteTodo };
+}
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   console.log("delete todo id is ", todoId)
 
-  var params = {
-    TableName: todosTable,
-    Key: {
-      "userId": 1,
-      "todoId": todoId
-    }
-  }
-  docClient.delete(params, function(err, data) {
-    if (err) {
-      console.error("unable to delete item", JSON.stringify(err, null, 2))      
-    }
-    else {
-      console.log("DeleteItem succeeded", JSON.stringify(data, null, 2))
-    }
-  })
+
+  const ret = await deleteTodo(todoId, "1")
+
+  console.log("return value from deleteTodo was ", ret)
+
+  // var params = {
+  //   TableName: todosTable,
+  //   Key: {
+  //     "userId": 1,
+  //     "todoId": todoId
+  //   }
+  // }
+  // docClient.delete(params, function(err, data) {
+  //   if (err) {
+  //     console.error("unable to delete item", JSON.stringify(err, null, 2))      
+  //   }
+  //   else {
+  //     console.log("DeleteItem succeeded", JSON.stringify(data, null, 2))
+  //   }
+  // })
 
   // const result = await docClient.delete({
   //   TableName: todosTable,
@@ -41,6 +58,6 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: JSON.stringify({"hello": "delete"})
+    body: JSON.stringify(ret)
   };
 }
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'x5n3x9nt4a'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev-v6`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'wudev.auth0.com',            // Auth0 domain
+  clientId: '1rjeLFNomhhCoa9nEP0SKt5WAGi4sciN',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
