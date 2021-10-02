dc3e0f15e2e7b3d3735ba5e1f025c9a89cc8c058
Noreen Wu <noreenwu@gmail.com>
2021-10-02 10:08:25 -0400

    moved iam permissions to function-specific GetTodos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -7,26 +7,8 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 import { getUserId } from '../utils'
 const todosTable = process.env.TODOS_TABLE
 
-
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   // TODO: Get all TODO items for a current user
-//   const response = {
-//     statusCode: 200,
-//     headers: {
-//       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-//     },
-//     body: JSON.stringify({
-//       message: 'Go Serverless v1.0! Your function executed successfully!',
-//       input: event,
-//     }),
-//   };
-//   return response;
-// }
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
-  // const result = await docClient.scan({
-  //   TableName: todosTable
-  // }).promise()
   const userId = getUserId(event)
 
   const result = await docClient
