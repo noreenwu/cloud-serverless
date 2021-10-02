6242130f5e2c428a62aef62b139df0ed89ef7593
Noreen Wu <noreenwu@gmail.com>
2021-10-01 23:25:13 -0400

    use todoId as key in signedUrl; update the DynamoDB record with attachment url, give permission to update DynamoDB in serverless.yml

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,11 @@
 import 'source-map-support/register'
 import { getUserId } from '../utils'
 
-const uuid = require('uuid');
+// const uuid = require('uuid');
 const AWS = require('aws-sdk')
+const todosTable = process.env.TODOS_TABLE
 
-// const docClient = new AWS.DynamoDB.DocumentClient();
+const docClient = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3({
   signatureVersion: 'v4'
 })
@@ -20,27 +21,42 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
   const userId = getUserId(event)
 
-  const imageId = uuid.v4()
-  const url = getUploadUrl(imageId)
+  // const imageId = uuid.v4()
+  const url = getUploadUrl(todoId)
 
   console.log("the signed url is: ", url)
   console.log("the userId is: ", userId)
   console.log("the todoId is: ", todoId)
+  
+  // update the DynamoDB entry for this todo
+  await docClient.update({
+      TableName: todosTable,
+      Key: { userId, todoId },
+      UpdateExpression: "set attachmentUrl=:URL",
+      ExpressionAttributeValues: {
+        ":URL": url.split("?")[0]
+      },
+      ReturnValues: "UPDATED_NEW"
+    })
+  .promise();  
+
+
   return {
     statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
+        todoId,
         uploadUrl: url
     })
   }
 }
 
-function getUploadUrl(imageId: string) {
+function getUploadUrl(todoId: string) {
     return s3.getSignedUrl('putObject', {
       Bucket: bucketName,
-      Key: imageId,
+      Key: todoId,
       Expires: urlExpiration
   })
 }
