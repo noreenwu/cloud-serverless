d56bd699977354e682725ffd0c5b0e835419f9a6
Noreen Wu <noreenwu@gmail.com>
2021-10-04 10:23:55 -0400

    fix imports, etc

--- backend/src/dataLayer/todosAccess.ts
+++ backend/src/dataLayer/todosAccess.ts
@@ -1,34 +1,33 @@
 
 const AWS = require('aws-sdk')
 import { TodoItem } from '../models/TodoItem'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 export class TodosAccess {
 
     constructor(
         private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
-        private readonly todosTable = process.env.TODOS_TABLE
-    )
+        private readonly todosTable = process.env.TODOS_TABLE) {
+        }
 
-    async getAllTodos(userId: string): Promise<TodoItem[]> {
-        console.log("Getting all todos")
+        async getAllTodos(userId: string): Promise<TodoItem[]> {
+            console.log("Getting all todos")
 
-        const result = await this.docClient
-            .query({
-            TableName: this.todosTable,
-            KeyConditionExpression: 'userId = :userId',
-            ExpressionAttributeValues: {
-                ':userId': userId
-            },
-          ScanIndexForward: false
-        }).promise()
+            const result = await this.docClient
+                .query({
+                TableName: this.todosTable,
+                KeyConditionExpression: 'userId = :userId',
+                ExpressionAttributeValues: {
+                    ':userId': userId
+                },
+            ScanIndexForward: false
+            }).promise()
 
-        console.log(result)
-        const items = result.Items
+            console.log(result)
+            const items = result.Items
 
-        return items as TodoItem[]
-    }
-
-
+            return items as TodoItem[]
+        }        
 }
 
 
