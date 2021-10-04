006f1eabdb6edf86e1b62e470166ad752995b848
Noreen Wu <noreenwu@gmail.com>
2021-10-03 21:15:50 -0400

    clean and comment

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,7 +1,6 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 import { verify, decode } from 'jsonwebtoken'
-// import { verify,  } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
@@ -9,11 +8,10 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
+
+// The jwksUrl URL can be pulled from the Auth0 page: Show Advanced Settings -> Endpoints -> JSON Web Key Set
+// It is used to verify the JWT token signature.
 const jwksUrl = 'https://wudev.auth0.com/.well-known/jwks.json'
-// const auth0Secret = process.env.AUTH_0_SECRET
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -66,8 +64,8 @@ const getSigningKey = async (jwkurl, kid) => {
   let keys  = res.data.keys;
   // since the keys is an array its possible to have many keys in case of cycling.
   const signingKeys = keys.filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
-      && key.kty === 'RSA' // We are only supporting RSA
-      && key.kid           // The `kid` must be present to be useful for later
+      && key.kty === 'RSA'                                 // We are only supporting RSA
+      && key.kid                                           // The `kid` must be present
       && key.x5c && key.x5c.length // Has useful public keys (we aren't using n or e)
     ).map(key => {
       return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
@@ -90,18 +88,11 @@ function certToPEM(cert) {
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
-  // TODO: Implement token verification
-  // Note: this works:
-  // return verify(token, auth0Secret) as JwtPayload
   const header = jwt.header
+
   let key = await getSigningKey(jwksUrl, header.kid)  
   return verify(token, key.publicKey, { algorithms: ['RS256'] }) as JwtPayload
 
-
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  // return undefined
 }
 
 function getToken(authHeader: string): string {
