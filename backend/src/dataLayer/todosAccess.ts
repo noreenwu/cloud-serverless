61aa1a29f18a7d4825fcd1d639b235e60c809314
Noreen Wu <noreenwu@gmail.com>
2021-10-04 09:58:23 -0400

    start of dataLayer

---
+++ backend/src/dataLayer/todosAccess.ts
@@ -0,0 +1,34 @@
+
+const AWS = require('aws-sdk')
+import { TodoItem } from '../models/TodoItem'
+
+export class TodosAccess {
+
+    constructor(
+        private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
+        private readonly todosTable = process.env.TODOS_TABLE
+    )
+
+    async getAllTodos(userId: string): Promise<TodoItem[]> {
+        console.log("Getting all todos")
+
+        const result = await this.docClient
+            .query({
+            TableName: this.todosTable,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': userId
+            },
+          ScanIndexForward: false
+        }).promise()
+
+        console.log(result)
+        const items = result.Items
+
+        return items as TodoItem[]
+    }
+
+
+}
+
+
