3af5bc6d64ab89b4d9b6bb6475bdf2edc903c05d
Noreen Wu <noreenwu@gmail.com>
2021-10-04 17:10:21 -0400

    refactor continued

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -1,8 +1,9 @@
-// import * as uuid from 'uuid'
+import * as uuid from 'uuid'
 
 import { TodoItem } from '../models/TodoItem'
 import { TodoAccess } from '../dataLayer/todosAccess'
-
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import { parseUserId } from '../auth/utils'
 
 const todoAccess = new TodoAccess();
 
@@ -10,3 +11,18 @@ export async function getAllTodos(userId: string): Promise<TodoItem[]> {
     return todoAccess.getAllTodos(userId)
 }
 
+export async function createGroup(newTodo: CreateTodoRequest, jwtToken: string): Promise<TodoItem> {
+    const todoId = uuid.v4()
+    const userId = parseUserId(jwtToken)
+
+    const todoAccess = new TodoAccess();
+
+    return await todoAccess.createTodo({
+        userId: userId,
+        todoId: todoId,
+        createdAt: Date.now().toString(),
+        name: newTodo.name,
+        dueDate: newTodo.dueDate,
+        done: false
+    })
+}
