31481ddf00a7ab01941f5debd8d0120df88fc339
Noreen Wu <noreenwu@gmail.com>
2021-10-04 23:19:08 -0400

    version number change

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 // The apiId here identifies the API in AWS APIGateway that is being called.
 // The apiEndpoint is the API's baseUrl.
-const apiId = 'rwyeixb5mk'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev-v10`
+const apiId = '40g2xhokvj'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev-v11`
 
 export const authConfig = {
   domain: 'wudev.auth0.com',                             // Auth0 domain
