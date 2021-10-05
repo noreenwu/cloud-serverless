ffd48dec8e36f8aabb1ee15e31eac2a9acf95797
Noreen Wu <noreenwu@gmail.com>
2021-10-04 22:42:44 -0400

    new version of getTodos uses business and data layers

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,43 +1,20 @@
-
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
-import { createLogger } from '../../utils/logger'
-
-const docClient = new AWS.DynamoDB.DocumentClient();
-import { getUserId } from '../utils'
-const todosTable = process.env.TODOS_TABLE
-const logger = createLogger('getTodos')
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getAllTodos } from '../../businessLogic/todos'
+import { getToken } from '../utils'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
-  const userId = getUserId(event)
+    const jwtToken = getToken(event)
+    const todos = await getAllTodos(jwtToken)
 
-  const result = await docClient
-    .query({
-      TableName: todosTable,
-      KeyConditionExpression: 'userId = :userId',
-      ExpressionAttributeValues: {
-        ':userId': userId
-      },
-      ScanIndexForward: false
-    }).promise()
-  
-  logger.info(`Pulled todo items for user ${userId}`, {
-    userId
-  });
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify(
-      {
-        message: "Hello: the getTodos() function executed successfully!",
-        input: event,
-        table: todosTable,
-        items: result.Items
-      },
-    ),
-  };
-};
+    return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            items: todos
+        })
+    }
+}
