5eb48ddeeff3eb9632264b3d51c438287d5c655a
Noreen Wu <noreenwu@gmail.com>
2021-10-04 22:49:04 -0400

    switched to layered deleteTodos

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,39 +1,21 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import { createLogger } from '../../utils/logger'
-import * as AWS from 'aws-sdk'
-import { getUserId } from '../utils'
 
-const docClient = new AWS.DynamoDB.DocumentClient();
-const todosTable = process.env.TODOS_TABLE
-const logger = createLogger('deleteTodo')
-
-const deleteTodo = async(todoId: string, userId: string) => {
-  const deleteTodo = await docClient.delete({
-      TableName: todosTable,
-      Key: { userId, todoId }
-  })
-  .promise();
-  return { Deleted: deleteTodo };
-}
+import { deleteTodo } from '../../businessLogic/todos'
+import { getToken } from '../utils'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
+    const todoId = event.pathParameters.todoId  
+    const jwtToken = getToken(event)
+    const deletedTodo = await deleteTodo(todoId, jwtToken)
 
-  const userId = getUserId(event)
-  const ret = await deleteTodo(todoId, userId)
-
-  logger.info('Todo was deleted', {
-    userId,
-    todoId
-  });
-
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify(ret)
-  };
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            Deleted: deletedTodo
+        })
+    }    
 }
